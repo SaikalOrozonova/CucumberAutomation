90fecf0e7de8df9aa45ec7585d0eb2b2aee01018
saikal <saikalorozonova2022@gmail.com>
2023-03-15 12:38:58 -0500

    Hi there, this is my first commit

---
+++ src/test/java/tests/Phone.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.openqa.selenium.devtools.v108.input.model.DragDataItem;
+import utilities.Driver;
+
+public class Phone {
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://google.com");
+    }
+    //git@github.com:SaikalOrozonova/CucumberAutomation.git
+}
